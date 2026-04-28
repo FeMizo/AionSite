ce6c978aebc32ee069da2ebf527d51ca663177e4
FeMizo <femiss0693@gmail.com>
2026-04-27 22:06:05 -0600

    :hammer: V1.17.28 Robots

--- app/robots.ts
+++ app/robots.ts
@@ -12,7 +12,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: "*",
-        allow: ["/", "/about", "/proyectos"],
         disallow: ["/admin/", "/api/"],
       },
     ],
